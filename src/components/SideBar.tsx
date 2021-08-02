c8fb4e0bb93a91235fb96d88a4fdc1412b4f9c7c
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-08-01 22:50:03 -0300

    refactor: componentization

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+
+import '../styles/movie-card.scss';
+import { Button } from './Button';
+interface GenreResponseProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[];
+  handleClickButton: (id: number) => any;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: GenreResponseProps): JSX.Element {
+  return <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id} />
+      ))}
+    </div>
+
+  </nav>;
 }
